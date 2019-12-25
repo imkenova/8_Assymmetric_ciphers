05de66d1e4b8c9c1fbfb33868073f5f6c09410d4
Елена Имкенова <lena_imkenova@MacBook-Air-Elena.local>
2019-12-25 20:38:04 +0300

    Осн. задания

--- server.py
+++ server.py
@@ -1,15 +1,44 @@
 import socket
 import pickle
+from random import randint
+
+def cript(a1, key):
+    a2 = [chr(ord(a1[i]) ^ key) for i in range(len(a1))]
+    return ''.join(a2)
+
+
+def send(conn, msg, K):
+    msg = cript(msg, K)
+    conn.send(pickle.dumps(msg))
+
+
+def recv(conn, K):
+    msg = pickle.loads(conn.recv(1024))
+    msg = cript(msg, K)
+    return msg
+
 
 HOST = '127.0.0.1'
 PORT = 8080
-
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+p, g, A = pickle.loads(msg)
+b = randint(10, 250)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+K = A ** b % p
+
+
+while True:
+    try:
+        msg = recv(conn, K)
+        print(msg)
+        send(conn, 'Сообщение успешно получено и расшифровано', K)
+    except EOFError:
+        break
 
 conn.close()
